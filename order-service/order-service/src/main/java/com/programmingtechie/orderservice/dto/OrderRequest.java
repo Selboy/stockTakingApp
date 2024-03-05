6e92c169037c4c33b3ee0fa4f3c4bc672b718675
Selboy <selpymahlangu@gmail.com>
2024-03-05 10:46:49 +0200

    product and order

---
+++ order-service/order-service/src/main/java/com/programmingtechie/orderservice/dto/OrderRequest.java
@@ -0,0 +1,11 @@
+package com.programmingtechie.orderservice.dto;
+
+import lombok.*;
+
+import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderRequest {
+private List<OrderLineItemsDto> orderLineItemsDtoList;
+}
